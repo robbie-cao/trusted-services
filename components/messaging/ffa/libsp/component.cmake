9c8ef92890448a81fed5c7d0facd44bd04452198
Imre Kis <imre.kis@arm.com>
2020-11-23 03:15:48 +0100

    libsp: Add memory descriptor handler functions.
    The patch contains functions for building and parsing the structures of
    memory transaction. These functions enable the user to describe the
    memory regions and their access specification to share/lend/donate
    and retrieve.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I927fbfc857b06a34a727be2d5c6d89ed7db4445c

--- components/messaging/ffa/libsp/component.cmake
+++ components/messaging/ffa/libsp/component.cmake
@@ -12,6 +12,7 @@ target_sources(${TGT} PRIVATE
 	"${CMAKE_CURRENT_LIST_DIR}/aarch64/ffa_syscalls_a64.S"
 	"${CMAKE_CURRENT_LIST_DIR}/ffa.c"
 	"${CMAKE_CURRENT_LIST_DIR}/ffa_interrupt_handler.c"
+	"${CMAKE_CURRENT_LIST_DIR}/ffa_memory_descriptors.c"
 	"${CMAKE_CURRENT_LIST_DIR}/sp_rxtx.c"
 	)
 
@@ -20,6 +21,7 @@ set_property(TARGET ${TGT} PROPERTY PUBLIC_HEADER
 	${CMAKE_CURRENT_LIST_DIR}/include/ffa_api_types.h
 	${CMAKE_CURRENT_LIST_DIR}/include/ffa_api.h
 	${CMAKE_CURRENT_LIST_DIR}/include/ffa_internal_api.h
+	${CMAKE_CURRENT_LIST_DIR}/include/ffa_memory_descriptors.h
 	${CMAKE_CURRENT_LIST_DIR}/include/sp_api_defines.h
 	${CMAKE_CURRENT_LIST_DIR}/include/sp_api_types.h
 	${CMAKE_CURRENT_LIST_DIR}/include/sp_api.h
