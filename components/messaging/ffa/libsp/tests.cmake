721104fedfac828690af6a318fb8d1d255f436b3
Imre Kis <imre.kis@arm.com>
2020-11-30 20:52:38 +0100

    libsp: Add mock assert implementation
    Assertion failures can be expected from tests and an exception-like
    behavior is implemented to prevent executing code after a failed
    assert.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I0ed0951baab40a449018d61926daded2f6a970ed

---
+++ components/messaging/ffa/libsp/tests.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2020-2021, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include(UnitTest)
+
+unit_test_add_suite(
+	NAME libsp_mock_assert
+	SOURCES
+		${CMAKE_CURRENT_LIST_DIR}/test/mock_assert.cpp
+		${CMAKE_CURRENT_LIST_DIR}/test/test_mock_assert.cpp
+	INCLUDE_DIRECTORIES
+		${CMAKE_CURRENT_LIST_DIR}/include/
+		${PROJECT_PATH}/components/common/utils/include
+	COMPILE_DEFINITIONS
+		-DARM64
+)
