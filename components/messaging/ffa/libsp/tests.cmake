f5f4a554d8d5e6917cbd31ad726e13e9ab055449
Imre Kis <imre.kis@arm.com>
2021-01-12 22:22:04 +0100

    libsp: Unit testing sp_memory_management.c
    Testing the functions of sp_memory_management.c using the mocked FF-A
    layer and SP RXTX implementation. The static functions of the file are
    tested by exporting them through c-picker.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: Ie9995c83b7d9ceb98527bac6685e6f9d555471af

--- components/messaging/ffa/libsp/tests.cmake
+++ components/messaging/ffa/libsp/tests.cmake
@@ -96,3 +96,33 @@ unit_test_add_suite(
 	COMPILE_DEFINITIONS
 		-DARM64
 )
+
+unit_test_add_suite(
+	NAME libsp_sp_memory_management
+	SOURCES
+		${CMAKE_CURRENT_LIST_DIR}/test/test_sp_memory_management.cpp
+		${CMAKE_CURRENT_LIST_DIR}/sp_memory_management.c
+		${CMAKE_CURRENT_LIST_DIR}/ffa_memory_descriptors.c
+		${CMAKE_CURRENT_LIST_DIR}/test/mock_assert.cpp
+		${CMAKE_CURRENT_LIST_DIR}/test/mock_ffa_api.cpp
+		${CMAKE_CURRENT_LIST_DIR}/test/mock_sp_rxtx.cpp
+	INCLUDE_DIRECTORIES
+		${CMAKE_CURRENT_LIST_DIR}/include/
+		${PROJECT_PATH}/components/common/utils/include
+	COMPILE_DEFINITIONS
+		-DARM64
+)
+
+unit_test_add_suite(
+	NAME libsp_sp_memory_management_internals
+	SOURCES
+		${CMAKE_CURRENT_LIST_DIR}/test/test_sp_memory_management_internals.cpp
+		${CMAKE_CURRENT_LIST_DIR}/test/sp_memory_management_internals.yml
+		${CMAKE_CURRENT_LIST_DIR}/ffa_memory_descriptors.c
+		${CMAKE_CURRENT_LIST_DIR}/test/mock_assert.cpp
+	INCLUDE_DIRECTORIES
+		${CMAKE_CURRENT_LIST_DIR}/include/
+		${PROJECT_PATH}/components/common/utils/include
+	COMPILE_DEFINITIONS
+		-DARM64
+)
