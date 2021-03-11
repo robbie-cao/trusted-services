robbie-cao/trusted-services

commit d80f856adf59dd606955ed78e3498649d131832a

Fix: CROSS_COMPILE env variable is ignored
The "arm-linux" deployments ignore the CROSS_COMPILE environment
variable which makes the build fail when the compiler is not on the
PATH.
This patch fixes this incorrect behavior and makes the "arm-linux" and
the "opteesp" deployments aligned on how the environment variable is
handled.
The policy is to prefer any value already in the cache. If the variable
is not in the cache, the value will be taken over from the
environment.

Change-Id: Id3cc4d6c23767d8ad9cb9657a582d6f70e191e37
Signed-off-by: Gyorgy Szing <Gyorgy.Szing@arm.com>

environments/arm-linux/default_toolchain_file.cmake
@@ -1,10 +1,12 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
-set(ENV{CROSS_COMPILE} "aarch64-linux-gnu-;aarch64-none-linux-gnu-")
+if(NOT CROSS_COMPILE AND NOT DEFINED ENV{CROSS_COMPILE})
+        set(CROSS_COMPILE "aarch64-elf-;aarch64-none-elf-;aarch64-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
+endif()
 
 set(CMAKE_CROSSCOMPILING True)
 set(CMAKE_SYSTEM_NAME Linux)

environments/opteesp/default_toolchain_file.cmake
@@ -1,12 +1,12 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
 #GNUARM v8 and v9 compilers use a different triplet.
-if(NOT DEFINED ENV{CROSS_COMPILE})
+if(NOT CROSS_COMPILE AND NOT DEFINED ENV{CROSS_COMPILE})
 	set(CROSS_COMPILE "aarch64-elf-;aarch64-none-elf-;aarch64-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
 endif()
 

commit daf2efdaa780a7963a84f89ced5892bd936d32ec

Fix: "arm-linux" builds allow wrong GCC compiler
Change 	d80f856adf59 introduced a bug. It makes deployments targeting
the "arm-linux" environment accept non Linux specific GCC binaries like
"aarch64-none-elf-gcc". Since these GCC builds are not bundled with a
standard library built with Linux support, compilation errors arise due
to missing Linux kernel headers.

In addition this change fixes the GCC prefix triplet list for the opeesp
environment and allows using GCC built with aarch64-none-linux
triplet.

Change-Id: Ideff9f53dfe9080cd0ba24f75279a81532badb04
Signed-off-by: Gyorgy Szing <Gyorgy.Szing@arm.com>

environments/arm-linux/default_toolchain_file.cmake
@@ -5,7 +5,7 @@
 #
 #-------------------------------------------------------------------------------
 if(NOT CROSS_COMPILE AND NOT DEFINED ENV{CROSS_COMPILE})
-        set(CROSS_COMPILE "aarch64-elf-;aarch64-none-elf-;aarch64-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
+        set(CROSS_COMPILE "aarch64-linux-gnu-;aarch64-none-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
 endif()
 
 set(CMAKE_CROSSCOMPILING True)

environments/opteesp/default_toolchain_file.cmake
@@ -7,7 +7,7 @@
 
 #GNUARM v8 and v9 compilers use a different triplet.
 if(NOT CROSS_COMPILE AND NOT DEFINED ENV{CROSS_COMPILE})
-	set(CROSS_COMPILE "aarch64-elf-;aarch64-none-elf-;aarch64-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
+	set(CROSS_COMPILE "aarch64-elf-;aarch64-none-elf-;aarch64-linux-gnu-;aarch64-none-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
 endif()
 
 set(CMAKE_CROSSCOMPILING True)