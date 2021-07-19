4118ec0f52850c4a756a0afe749bfa1be387ed8e
Imre Kis <imre.kis@arm.com>
2021-07-19 17:11:15 +0200

    Add FF-A RPC endpoint implementation for SEL1 SPs
    Implement FF-A RPC endpoint for OP-TEE based SEL1 SPs (Pseudo-SPs).
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I2d3c4238b40a7270a776be31f1be3ad7cf77bf40

---
+++ components/rpc/ffarpc/endpoint/sel1_sp/sel1_sp_ffarpc_call_ep.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (C) 2020-2022, Arm Limited
+ */
+
+#ifndef FFA_CALL_EP_H
+#define FFA_CALL_EP_H
+
+#include <components/rpc/common/endpoint/rpc_interface.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef NUM_MAX_SESS
+#define NUM_MAX_SESS (16)
+#endif
+
+struct thread_smc_args;
+
+struct ffa_call_ep {
+	struct rpc_interface *iface;
+	unsigned long shmem_buf_handle[NUM_MAX_SESS];
+	uint8_t *shmem_buf[NUM_MAX_SESS];
+	size_t shmem_buf_size[NUM_MAX_SESS];
+	uint16_t src_id[NUM_MAX_SESS];
+};
+
+void ffa_call_ep_init(struct ffa_call_ep *ffa_call_ep,
+		      struct rpc_interface *iface);
+void ffa_call_ep_receive(struct ffa_call_ep *call_ep,
+			 const struct thread_smc_args *req_msg,
+			 struct thread_smc_args *resp_msg);
+void *ffa_call_ep_get_buffer(struct ffa_call_ep *call_ep, uint16_t source_id,
+			     size_t *buffer_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FFA_CALL_EP_H */
