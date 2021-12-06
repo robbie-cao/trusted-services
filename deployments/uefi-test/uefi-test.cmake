1911a12447a3aafe6baf25ad854306e6b26d41d5
Julian Hall <julian.hall@arm.com>
2021-12-06 15:20:12 +0000

    Add uefi-test deployment
    Adds a new deployment for building and running service level tests
    for UEFI SMM services. Tests may be run against StMM, smm-gateway
    or any other similar secure-world uefi service provider.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: Ic0e16dff51ef76ddd1f4dea37a4a55b029edd696

---
+++ deployments/uefi-test/uefi-test.cmake
@@ -0,0 +1,52 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#  The base build file shared between deployments of 'uefi-test' for
+#  different environments.  Used for running end-to-end service-level tests
+#  against SMM service providers that implement UEFI services such as smm
+#  variable.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#  Use libts for locating and accessing services. An appropriate version of
+#  libts will be imported for the enviroment in which service tests are
+#  deployed.
+#-------------------------------------------------------------------------------
+include(${TS_ROOT}/deployments/libts/libts-import.cmake)
+target_link_libraries(uefi-test PRIVATE libts)
+
+#-------------------------------------------------------------------------------
+#  Components that are common accross all deployments
+#
+#-------------------------------------------------------------------------------
+add_components(
+	TARGET "uefi-test"
+	BASE_DIR ${TS_ROOT}
+	COMPONENTS
+		"components/service/smm_variable/client/cpp"
+		"components/service/smm_variable/test/service"
+)
+
+#-------------------------------------------------------------------------------
+#  Components used from external projects
+#
+#-------------------------------------------------------------------------------
+
+# Nanopb
+include(${TS_ROOT}/external/nanopb/nanopb.cmake)
+target_link_libraries(uefi-test PRIVATE nanopb::protobuf-nanopb-static)
+protobuf_generate_all(TGT "uefi-test" NAMESPACE "protobuf" BASE_DIR "${TS_ROOT}/protocols")
+
+#-------------------------------------------------------------------------------
+#  Define install content.
+#
+#-------------------------------------------------------------------------------
+if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+	set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/install CACHE PATH "location to install build output to." FORCE)
+endif()
+install(TARGETS uefi-test RUNTIME DESTINATION ${TS_ENV}/bin)
