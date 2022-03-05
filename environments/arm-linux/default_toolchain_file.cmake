748838e6a0901ca9bb5add13dc2239efa7a7f21f
Gyorgy Szing <Gyorgy.Szing@arm.com>
2022-03-05 04:17:34 +0000

    Fix arm-linux default toolchain file
    The toolchain file was incorrectly setting the xx_INIT variables and
    thus losing value set in initial cache file.
    This patch switches to string(APPEND ...) to avoid overwriting data.
    
    Change-Id: Ie8a0b2d624fdcc859861a62199f6ccb8df51e087
    Signed-off-by: Gyorgy Szing <Gyorgy.Szing@arm.com>

--- environments/arm-linux/default_toolchain_file.cmake
+++ environments/arm-linux/default_toolchain_file.cmake
@@ -1,19 +1,19 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 if(NOT CROSS_COMPILE AND NOT DEFINED ENV{CROSS_COMPILE})
-        set(CROSS_COMPILE "aarch64-linux-gnu-;aarch64-none-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
+	set(CROSS_COMPILE "aarch64-linux-gnu-;aarch64-none-linux-gnu-" CACHE STRING "List of GCC prefix triplets to use.")
 endif()
 
 set(CMAKE_CROSSCOMPILING True)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_FLAGS_INIT "-fdiagnostics-show-option -gdwarf-2 -mstrict-align -O0 -DARM64=1")
-set(CMAKE_CXX_FLAGS_INIT "-fdiagnostics-show-option -gdwarf-2 -mstrict-align -O0 -DARM64=1")
+string(APPEND CMAKE_C_FLAGS_INIT " -fdiagnostics-show-option -gdwarf-2 -mstrict-align -O0 -DARM64=1")
+string(APPEND CMAKE_CXX_FLAGS_INIT " -fdiagnostics-show-option -gdwarf-2 -mstrict-align -O0 -DARM64=1")
 
 include($ENV{TS_ROOT}/tools/cmake/compiler/GCC.cmake REQUIRED)
 include($ENV{TS_ROOT}/tools/cmake/compiler/config_iface.cmake REQUIRED)
