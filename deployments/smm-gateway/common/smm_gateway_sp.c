85226922b6fdd5494c7c662fd0e1c89a83fac486
Imre Kis <imre.kis@arm.com>
2022-07-05 16:44:11 +0200

    Refactor smm_gateway_sp.c
    Add error handling and logging.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: Ib1e2975e42bc4c32592a002b6d2d3b109b0b2408

--- deployments/smm-gateway/common/smm_gateway_sp.c
+++ deployments/smm-gateway/common/smm_gateway_sp.c
@@ -3,24 +3,25 @@
  * Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
  */
 
-#include <rpc/ffarpc/endpoint/ffarpc_call_ep.h>
+#include "rpc/ffarpc/endpoint/ffarpc_call_ep.h"
 #include "smm_gateway.h"
-#include <config/ramstore/config_ramstore.h>
+#include "config/ramstore/config_ramstore.h"
 #include "config/interface/config_store.h"
-#include <config/loader/sp/sp_config_loader.h>
+#include "config/loader/sp/sp_config_loader.h"
 #include "components/rpc/mm_communicate/endpoint/sp/mm_communicate_call_ep.h"
 #include "components/service/smm_variable/frontend/mm_communicate/smm_variable_mm_service.h"
 #include "platform/interface/memory_region.h"
 #include "protocols/common/mm/mm_smc.h"
-#include <ffa_api.h>
-#include <sp_api.h>
-#include <sp_messaging.h>
-#include <sp_rxtx.h>
-#include <trace.h>
+#include "ffa_api.h"
+#include "sp_api.h"
+#include "sp_discovery.h"
+#include "sp_messaging.h"
+#include "sp_rxtx.h"
+#include "trace.h"
 
 #define CONFIG_NAME_MM_COMM_BUFFER_REGION	"mm-comm-buffer"
 
-static int sp_init(uint16_t *own_sp_id);
+static bool sp_init(uint16_t *own_sp_id);
 
 void __noreturn sp_main(struct ffa_init_info *init_info)
 {
@@ -32,15 +33,23 @@ void __noreturn sp_main(struct ffa_init_info *init_info)
 	struct ffa_direct_msg req_msg = { 0 };
 	struct ffa_direct_msg resp_msg = { 0 };
 	uint16_t own_id = 0;
+	ffa_result result = FFA_DENIED;
 
 	static const EFI_GUID smm_variable_guid = SMM_VARIABLE_GUID;
 
 	/* Boot phase */
-	if (sp_init(&own_id) != 0) goto fatal_error;
+	if (!sp_init(&own_id)) {
+		EMSG("Failed to init SP");
+		goto fatal_error;
+	}
 
 	/* Load any dynamic configuration */
 	config_ramstore_init();
-	sp_config_load(init_info);
+
+	if (!sp_config_load(init_info)) {
+		EMSG("Failed to load SP config");
+		goto fatal_error;
+	}
 
 	if (!config_store_query(CONFIG_CLASSIFIER_MEMORY_REGION, CONFIG_NAME_MM_COMM_BUFFER_REGION,
 				0, &mm_comm_buffer_region, sizeof(mm_comm_buffer_region))) {
@@ -50,22 +59,36 @@ void __noreturn sp_main(struct ffa_init_info *init_info)
 
 	/* Initialize service layer and associate with RPC endpoint */
 	gateway_iface = smm_gateway_create(own_id);
+	if (!gateway_iface) {
+		EMSG("Failed to create SMM gateway");
+		goto fatal_error;
+	}
 
 	/* Initialize SMM variable MM service */
 	smm_var_service_interface = smm_variable_mm_service_init(&smm_var_service, gateway_iface);
+	if (!smm_var_service_interface) {
+		EMSG("Failed to init SMM variable MM service");
+		goto fatal_error;
+	}
 
 	/* Initialize MM communication layer */
 	if (!mm_communicate_call_ep_init(&mm_communicate_call_ep,
 					 (void *)mm_comm_buffer_region.base_addr,
-					 mm_comm_buffer_region.region_size))
+					 mm_comm_buffer_region.region_size)) {
+		EMSG("Failed to init MM communicate call EP");
 		goto fatal_error;
+	}
 
 	/* Attach SMM variable service to MM communication layer */
 	mm_communicate_call_ep_attach_service(&mm_communicate_call_ep, &smm_variable_guid,
 					      smm_var_service_interface);
 
 	/* End of boot phase */
-	ffa_msg_wait(&req_msg);
+	result = ffa_msg_wait(&req_msg);
+	if (result != FFA_OK) {
+		EMSG("Failed to send message wait %d", result);
+		goto fatal_error;
+	}
 
 	while (1) {
 		if (FFA_IS_32_BIT_FUNC(req_msg.function_id)) {
@@ -78,11 +101,19 @@ void __noreturn sp_main(struct ffa_init_info *init_info)
 
 		mm_communicate_call_ep_receive(&mm_communicate_call_ep, &req_msg, &resp_msg);
 
-		ffa_msg_send_direct_resp_64(req_msg.destination_id,
-					 req_msg.source_id, resp_msg.args.args64[0],
-					 resp_msg.args.args64[1], resp_msg.args.args64[2],
-					 resp_msg.args.args64[3], resp_msg.args.args64[4],
-					 &req_msg);
+		result = ffa_msg_send_direct_resp_64(req_msg.destination_id,
+						  req_msg.source_id, resp_msg.args.args64[0],
+						  resp_msg.args.args64[1], resp_msg.args.args64[2],
+						  resp_msg.args.args64[3], resp_msg.args.args64[4],
+						  &req_msg);
+		if (result != FFA_OK) {
+			EMSG("Failed to send direct response %d", result);
+			result = ffa_msg_wait(&req_msg);
+			if (result != FFA_OK) {
+				EMSG("Failed to send message wait %d", result);
+				goto fatal_error;
+			}
+		}
 	}
 
 fatal_error:
@@ -96,21 +127,23 @@ void sp_interrupt_handler(uint32_t interrupt_id)
 	(void)interrupt_id;
 }
 
-static int sp_init(uint16_t *own_sp_id)
+static bool sp_init(uint16_t *own_id)
 {
-	int status = -1;
-	ffa_result ffa_res;
-	sp_result sp_res;
+	sp_result sp_res = SP_RESULT_INTERNAL_ERROR;
 	static uint8_t tx_buffer[4096] __aligned(4096);
 	static uint8_t rx_buffer[4096] __aligned(4096);
 
 	sp_res = sp_rxtx_buffer_map(tx_buffer, rx_buffer, sizeof(rx_buffer));
-	if (sp_res == SP_RESULT_OK) {
-		ffa_res = ffa_id_get(own_sp_id);
-		if (ffa_res == FFA_OK) {
-			status = 0;
-		}
+	if (sp_res != SP_RESULT_OK) {
+		EMSG("Failed to map RXTX buffers: %d", sp_res);
+		return false;
 	}
 
-	return status;
+	sp_res = sp_discovery_own_id_get(own_id);
+	if (sp_res != SP_RESULT_OK) {
+		EMSG("Failed to query own ID: %d", sp_res);
+		return false;
+	}
+
+	return true;
 }
