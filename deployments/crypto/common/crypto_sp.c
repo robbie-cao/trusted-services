6ef4d0d37bdc0ad4e3959abd748d672cefdf7e40
Imre Kis <imre.kis@arm.com>
2022-07-05 16:43:17 +0200

    Refactor crypto_sp.c
    Add error handling and logging.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: Ifea5eeb88093b3b742494d12d79608f1571903ab

--- deployments/crypto/common/crypto_sp.c
+++ deployments/crypto/common/crypto_sp.c
@@ -3,60 +3,93 @@
  * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
  */
 
+#include "rpc/ffarpc/endpoint/ffarpc_call_ep.h"
+#include "service/secure_storage/factory/storage_factory.h"
+#include "service/crypto/factory/crypto_provider_factory.h"
+#include "service/crypto/backend/mbedcrypto/mbedcrypto_backend.h"
+#include "protocols/rpc/common/packed-c/status.h"
+#include "config/ramstore/config_ramstore.h"
+#include "config/loader/sp/sp_config_loader.h"
+#include "sp_api.h"
+#include "sp_discovery.h"
+#include "sp_messaging.h"
+#include "sp_rxtx.h"
+#include "trace.h"
 
-#include <rpc/ffarpc/endpoint/ffarpc_call_ep.h>
-#include <service/secure_storage/factory/storage_factory.h>
-#include <service/crypto/factory/crypto_provider_factory.h>
-#include <service/crypto/backend/mbedcrypto/mbedcrypto_backend.h>
-#include <protocols/rpc/common/packed-c/status.h>
-#include <config/ramstore/config_ramstore.h>
-#include <config/loader/sp/sp_config_loader.h>
-#include <ffa_api.h>
-#include <sp_api.h>
-#include <sp_messaging.h>
-#include <sp_rxtx.h>
-#include <trace.h>
-
-static int sp_init(uint16_t *own_sp_id);
+static bool sp_init(uint16_t *own_sp_id);
 
 void __noreturn sp_main(struct ffa_init_info *init_info)
 {
-	struct crypto_provider *crypto_provider;
-	struct ffa_call_ep ffarpc_call_ep;
-	struct rpc_interface *crypto_iface;
+	struct crypto_provider *crypto_provider = NULL;
+	struct ffa_call_ep ffarpc_call_ep = { 0 };
+	struct rpc_interface *crypto_iface = NULL;
 	struct sp_msg req_msg = { 0 };
 	struct sp_msg resp_msg = { 0 };
-	struct storage_backend *storage_backend;
+	struct storage_backend *storage_backend = NULL;
 	uint16_t own_id = 0;
+	psa_status_t psa_status = PSA_ERROR_GENERIC_ERROR;
+	sp_result result = SP_RESULT_INTERNAL_ERROR;
 
 	/* Boot phase */
-	if (sp_init(&own_id) != 0) goto fatal_error;
+	if (!sp_init(&own_id)) {
+		EMSG("Failed to init SP");
+		goto fatal_error;
+	}
 
 	config_ramstore_init();
-	sp_config_load(init_info);
+
+	if (!sp_config_load(init_info)) {
+		EMSG("Failed to load SP config");
+		goto fatal_error;
+	}
 
 	/* Create a storage backend for persistent key storage - prefer ITS */
 	storage_backend = storage_factory_create(storage_factory_security_class_INTERNAL_TRUSTED);
-	if (!storage_backend) goto fatal_error;
+	if (!storage_backend) {
+		EMSG("Failed to create storage factory");
+		goto fatal_error;
+	}
 
 	/* Initialize the crypto service */
-	crypto_iface = NULL;
+	psa_status = mbedcrypto_backend_init(storage_backend, 0);
+	if (psa_status != PSA_SUCCESS) {
+		EMSG("Failed to init Mbed TLS backend: %d", psa_status);
+		goto fatal_error;
+	}
 
-	if (mbedcrypto_backend_init(storage_backend, 0) == PSA_SUCCESS) {
+	crypto_provider = crypto_provider_factory_create();
+	if (!crypto_provider) {
+		EMSG("Failed to create crypto provider factory");
+		goto fatal_error;
+	}
 
-		crypto_provider = crypto_provider_factory_create();
-		crypto_iface = service_provider_get_rpc_interface(&crypto_provider->base_provider);
+	crypto_iface = service_provider_get_rpc_interface(&crypto_provider->base_provider);
+	if (!crypto_iface) {
+		EMSG("Failed to create service provider RPC interface");
+		goto fatal_error;
 	}
 
 	ffa_call_ep_init(&ffarpc_call_ep, crypto_iface, own_id);
 
 	/* End of boot phase */
-	sp_msg_wait(&req_msg);
+	result = sp_msg_wait(&req_msg);
+	if (result != SP_RESULT_OK) {
+		EMSG("Failed to send message wait %d", result);
+		goto fatal_error;
+	}
 
 	while (1) {
 		ffa_call_ep_receive(&ffarpc_call_ep, &req_msg, &resp_msg);
 
-		sp_msg_send_direct_resp(&resp_msg, &req_msg);
+		result = sp_msg_send_direct_resp(&resp_msg, &req_msg);
+		if (result != SP_RESULT_OK) {
+			EMSG("Failed to send direct response %d", result);
+			result = sp_msg_wait(&req_msg);
+			if (result != SP_RESULT_OK) {
+				EMSG("Failed to send message wait %d", result);
+				goto fatal_error;
+			}
+		}
 	}
 
 fatal_error:
@@ -70,21 +103,23 @@ void sp_interrupt_handler(uint32_t interrupt_id)
 	(void)interrupt_id;
 }
 
-static int sp_init(uint16_t *own_sp_id)
+static bool sp_init(uint16_t *own_id)
 {
-	int status = -1;
-	ffa_result ffa_res;
-	sp_result sp_res;
+	sp_result sp_res = SP_RESULT_INTERNAL_ERROR;
 	static uint8_t tx_buffer[4096] __aligned(4096);
 	static uint8_t rx_buffer[4096] __aligned(4096);
 
 	sp_res = sp_rxtx_buffer_map(tx_buffer, rx_buffer, sizeof(rx_buffer));
-	if (sp_res == SP_RESULT_OK) {
-		ffa_res = ffa_id_get(own_sp_id);
-		if (ffa_res == FFA_OK) {
-			status = 0;
-		}
+	if (sp_res != SP_RESULT_OK) {
+		EMSG("Failed to map RXTX buffers: %d", sp_res);
+		return false;
 	}
 
-	return status;
+	sp_res = sp_discovery_own_id_get(own_id);
+	if (sp_res != SP_RESULT_OK) {
+		EMSG("Failed to query own ID: %d", sp_res);
+		return false;
+	}
+
+	return true;
 }
