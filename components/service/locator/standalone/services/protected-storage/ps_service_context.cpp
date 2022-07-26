af9103596ecd79cf59f043998e1249dfeee11d71
Julian Hall <julian.hall@arm.com>
2022-07-26 14:42:34 +0100

    Integrate block_store backend into PS standalone service context
    The standalone service context for the Protected Storage service
    has been modified to use the Secure Flash Store with a block store
    backend. This makes service testing in the linux_pc environment
    closer to testing on real hardware by including block_storage
    components in the service deployment.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: Ib7ed3e323958b4948a80526c2163cc8b55d4ed74

--- components/service/locator/standalone/services/protected-storage/ps_service_context.cpp
+++ components/service/locator/standalone/services/protected-storage/ps_service_context.cpp
@@ -1,15 +1,19 @@
 /*
- * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include "ps_service_context.h"
+#include "service/block_storage/factory/client/block_store_factory.h"
+#include "service/block_storage/config/ref/ref_partition_configurator.h"
+#include "service/secure_storage/backend/secure_flash_store/secure_flash_store.h"
 
 ps_service_context::ps_service_context(const char *sn) :
-    standalone_service_context(sn),
-    m_storage_provider(),
-    m_mock_store()
+	standalone_service_context(sn),
+	m_storage_provider(),
+	m_sfs_flash_adapter(),
+	m_block_store(NULL)
 {
 
 }
@@ -21,14 +25,36 @@ ps_service_context::~ps_service_context()
 
 void ps_service_context::do_init()
 {
-    struct storage_backend *storage_backend = mock_store_init(&m_mock_store);
-    struct rpc_interface *storage_ep = secure_storage_provider_init(&m_storage_provider, storage_backend);
+	struct uuid_octets guid;
+	const struct sfs_flash_info_t *flash_info = NULL;
 
-    standalone_service_context::set_rpc_interface(storage_ep);
+	uuid_parse_to_octets(REF_PARTITION_2_GUID, guid.octets, sizeof(guid.octets));
+
+	m_block_store = client_block_store_factory_create(
+		"sn:trustedfirmware.org:block-storage:0");
+
+	psa_status_t status = sfs_flash_block_store_adapter_init(
+		&m_sfs_flash_adapter,
+		0,
+		m_block_store,
+		&guid,
+		MIN_FLASH_BLOCK_SIZE,
+		MAX_NUM_FILES,
+		&flash_info);
+
+	if (status == PSA_SUCCESS) {
+
+		struct storage_backend *storage_backend = sfs_init(flash_info);
+		struct rpc_interface *storage_ep = secure_storage_provider_init(
+			&m_storage_provider, storage_backend);
+
+		standalone_service_context::set_rpc_interface(storage_ep);
+	}
 }
 
 void ps_service_context::do_deinit()
 {
-    secure_storage_provider_deinit(&m_storage_provider);
-    mock_store_deinit(&m_mock_store);
+	secure_storage_provider_deinit(&m_storage_provider);
+	client_block_store_factory_destroy(m_block_store);
+	m_block_store = NULL;
 }
