8ad60435d3978a61286d6f792a9c4d50496fb78f
Julian Hall <julian.hall@arm.com>
2022-08-15 14:11:47 +0100

    Prototype alternative deployment structure
    Prototypes an alternative deployment directory structure for the
    block-storage service to improve support for different build
    configurations for different platforms. The new structure
    reflects the three layers for app, infra and env to improve
    reuse across multiple build configurations. The infra
    (infrastructure) layer significantly improves flexibility
    to realize app platform service dependencies in different
    ways.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: I0f91fd4b6f333eb3cb6a3a5e4206815bc3532fac

--- deployments/block-storage/block-storage.cmake
+++ deployments/block-storage/block-storage.cmake
@@ -3,36 +3,21 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
+# Common components used for any deployment of the block-storage service
+# provider.
 #-------------------------------------------------------------------------------
 
 add_components(TARGET "block-storage"
 	BASE_DIR ${TS_ROOT}
 	COMPONENTS
-		"components/common/fdt"
 		"components/common/tlv"
 		"components/common/uuid"
 		"components/common/endian"
-		"components/common/trace"
-		"components/common/utils"
-		"components/config/ramstore"
-		"components/config/loader/sp"
-		"components/messaging/ffa/libsp"
-		"components/rpc/common/interface"
-		"components/rpc/ffarpc/endpoint"
 		"components/service/common/include"
 		"components/service/common/provider"
 		"components/service/block_storage/block_store"
-		"components/service/block_storage/block_store/device"
-		"components/service/block_storage/block_store/device/ram"
-		"components/service/block_storage/block_store/partitioned"
 		"components/service/block_storage/provider"
 		"components/service/block_storage/provider/serializer/packed-c"
-		"components/service/block_storage/config/ref"
-		"components/service/block_storage/factory/ref_ram"
-)
-
-target_sources(block-storage PRIVATE
-	${CMAKE_CURRENT_LIST_DIR}/common/block_storage_sp.c
 )
 
 #################################################################
