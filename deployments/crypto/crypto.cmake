64f1d87507a3a63917b8c65c7e980cdfa0b0ec7d
Julian Hall <julian.hall@arm.com>
2022-09-05 16:52:40 +0100

    Refactor crypto deployment structure
    Refactors the crypto service deployment build and directory structure
    to allow for alternative build configurations.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: I8e7422fef7afac080a3d59f18807755db0d65c3d

--- deployments/crypto/crypto.cmake
+++ deployments/crypto/crypto.cmake
@@ -8,19 +8,9 @@
 add_components(TARGET "crypto"
 	BASE_DIR ${TS_ROOT}
 	COMPONENTS
-		"components/common/fdt"
 		"components/common/tlv"
-		"components/common/trace"
-		"components/common/utils"
-		"components/config/ramstore"
-		"components/config/loader/sp"
-		"components/messaging/ffa/libsp"
-		"components/rpc/ffarpc/endpoint"
-		"components/rpc/ffarpc/caller/sp"
-		"components/rpc/common/caller"
 		"components/rpc/common/interface"
 		"components/service/common/include"
-		"components/service/common/client"
 		"components/service/common/serializer/protobuf"
 		"components/service/common/provider"
 		"components/service/discovery/provider"
@@ -40,19 +30,8 @@ add_components(TARGET "crypto"
 		"components/service/crypto/provider/extension/aead/serializer/packed-c"
 		"components/service/crypto/factory/full"
 		"components/service/crypto/backend/mbedcrypto"
-		"components/service/crypto/backend/mbedcrypto/trng_adapter/platform"
-		"components/service/secure_storage/include"
-		"components/service/secure_storage/frontend/psa/its"
-		"components/service/secure_storage/backend/secure_storage_client"
-		"components/service/secure_storage/backend/null_store"
-		"components/service/secure_storage/factory/sp/rot_store"
 		"protocols/rpc/common/packed-c"
-		"protocols/service/secure_storage/packed-c"
 		"protocols/service/crypto/protobuf"
-)
-
-target_sources(crypto PRIVATE
-	${CMAKE_CURRENT_LIST_DIR}/common/crypto_sp.c
 )
 
 #-------------------------------------------------------------------------------
@@ -61,12 +40,12 @@ target_sources(crypto PRIVATE
 #-------------------------------------------------------------------------------
 
 # Nanopb
-include(../../../external/nanopb/nanopb.cmake)
+include(${TS_ROOT}/external/nanopb/nanopb.cmake)
 target_link_libraries(crypto PRIVATE nanopb::protobuf-nanopb-static)
 protobuf_generate_all(TGT "crypto" NAMESPACE "protobuf" BASE_DIR "${TS_ROOT}/protocols")
 
 # Mbed TLS provides libmbedcrypto
-include(../../../external/MbedTLS/MbedTLS.cmake)
+include(${TS_ROOT}/external/MbedTLS/MbedTLS.cmake)
 target_link_libraries(crypto PRIVATE mbedcrypto)
 target_link_libraries(mbedcrypto INTERFACE stdlib::c)
 
