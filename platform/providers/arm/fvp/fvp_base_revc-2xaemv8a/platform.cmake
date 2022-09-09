ebf3a56056f17415ec2a4ab4ac305a41ba0f9742
Julian Hall <julian.hall@arm.com>
2022-09-09 17:07:52 +0100

    Add tf-a external component
    Allows code from tf-a to be compiled into TS deployments. tf-a includes
    many components that can be reused in TS deployments, particularly in
    the area of storage access and firmware update. This commit adds a
    new tf-a external component which can be used to make tf-a components
    available in TS builds. To provide a pattern for how tf-a components
    can be used, a CFI NOR flash driver has been added. This shows how
    the a component within the TS tree is used to reference source files
    from the fetched tf-a repo. The CFI driver can be built but has not
    actually been tested so more work is almost certainly needed to make
    it usable. Even in its current state, the driver provides a reference
    for how other components from tf-a can be reused. To enable a full
    build to be tested that includes tf-a dependencies, the cfi-flash-optee
    block-storage build configuartion has been added.
    
    This allows
    reuse of platform level drivers and code from TF-A. The first TF-A
    code to reuse is a CFI NOR flash driver which is compatible with fvp
    base platforms.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: I69dacab1f4a0b2ab894e4130a0b54cff2a914563

--- platform/providers/arm/fvp/fvp_base_revc-2xaemv8a/platform.cmake
+++ platform/providers/arm/fvp/fvp_base_revc-2xaemv8a/platform.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -19,4 +19,8 @@ get_property(_platform_driver_dependencies TARGET ${TGT}
 #-------------------------------------------------------------------------------
 if ("trng" IN_LIST _platform_driver_dependencies)
 	include(${TS_ROOT}/platform/drivers/arm/juno_trng/driver.cmake)
+endif()
+
+if ("secure-nor-flash" IN_LIST _platform_driver_dependencies)
+	include(${TS_ROOT}/platform/drivers/tf-a/drivers/cfi/v2m/v2m_flash.cmake)
 endif()
