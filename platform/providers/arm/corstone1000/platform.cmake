eb388046acd7893d7a2dd60957df79d0f6d579fd
Rui Miguel Silva <rui.silva@linaro.org>
2022-10-11 10:46:10 +0100

    plat: corstone1000: change default smm values
    Smm gateway uses SE proxy to route the calls for any NV
    storage so set the NV_STORE_SN.
    Change the storage index uid because TF-M in the secure
    enclave reserves the default value (0x1) to some internal
    operation.
    
    Signed-off-by: Vishnu Banavath <vishnu.banavath@arm.com>
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>
    Change-Id: Ic995cb935d28c5e02cc18928f262815782cc3f0b

--- platform/providers/arm/corstone1000/platform.cmake
+++ platform/providers/arm/corstone1000/platform.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2021-2023, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -8,3 +8,9 @@
 
 # include MHU driver
 include(${TS_ROOT}/platform/drivers/arm/mhu_driver/component.cmake)
+
+target_compile_definitions(${TGT} PRIVATE
+	SMM_GATEWAY_NV_STORE_SN="sn:ffa:46bb39d1-b4d9-45b5-88ff-040027dab249:1"
+	SMM_VARIABLE_INDEX_STORAGE_UID=0x787
+	SMM_GATEWAY_MAX_UEFI_VARIABLES=100
+)
