6416f40fa33aa3a4e84239b31e6a9049360058be
Julian Hall <julian.hall@arm.com>
2022-12-13 18:42:56 +0000

    Add update rollback tests
    Adds update agent tests that check different update rollback
    scenarios. An update may be rollec back to the previous version
    while in the trial state.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: Icb92bd74644c3d16ec35c01ed49c5771ef513084

--- components/service/fwu/test/ref_scenarios/component.cmake
+++ components/service/fwu/test/ref_scenarios/component.cmake
@@ -13,4 +13,5 @@ target_sources(${TGT} PRIVATE
 	"${CMAKE_CURRENT_LIST_DIR}/invalid_behaviour_tests.cpp"
 	"${CMAKE_CURRENT_LIST_DIR}/update_scenario_tests.cpp"
 	"${CMAKE_CURRENT_LIST_DIR}/power_failure_tests.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/rollback_tests.cpp"
 	)
