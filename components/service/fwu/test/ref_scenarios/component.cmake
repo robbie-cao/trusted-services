414ee5993d8fcf59c7f1a7262289bc6e45ab95fc
Julian Hall <julian.hall@arm.com>
2023-01-04 14:22:31 +0000

    Add oversize image tests
    Adds tests that check that an attempt to install an update image that
    is too big for storage is detected and that the update transaction
    is not allowed to proceed to the trial state.
    
    Signed-off-by: Julian Hall <julian.hall@arm.com>
    Change-Id: Ife7b86870daa7a703005194a73243a3d80a21b1a

--- components/service/fwu/test/ref_scenarios/component.cmake
+++ components/service/fwu/test/ref_scenarios/component.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2022-2023, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -14,4 +14,5 @@ target_sources(${TGT} PRIVATE
 	"${CMAKE_CURRENT_LIST_DIR}/update_scenario_tests.cpp"
 	"${CMAKE_CURRENT_LIST_DIR}/power_failure_tests.cpp"
 	"${CMAKE_CURRENT_LIST_DIR}/rollback_tests.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/oversize_image_tests.cpp"
 	)
