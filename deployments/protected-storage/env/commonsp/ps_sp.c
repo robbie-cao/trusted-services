3ed2e051e2c6212d5f28bf85eeaa2e6c7ffec96c
Imre Kis <imre.kis@arm.com>
2023-06-28 14:19:31 +0200

    Update RPC layer initialization in PS SP
    Change ffa_call_ep to ts_rpc_endpoint_sp.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I1c08c9c6fbfdee424752b437de9b0ba667b84592

--- deployments/protected-storage/env/commonsp/ps_sp.c
+++ deployments/protected-storage/env/commonsp/ps_sp.c
@@ -4,10 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "components/rpc/common/endpoint/rpc_interface.h"
-#include "components/rpc/ffarpc/endpoint/ffarpc_call_ep.h"
+#include "components/rpc/common/endpoint/rpc_service_interface.h"
+#include "components/rpc/ts_rpc/endpoint/sp/ts_rpc_endpoint_sp.h"
 #include "components/service/secure_storage/factory/storage_factory.h"
 #include "components/service/secure_storage/frontend/secure_storage_provider/secure_storage_provider.h"
+#include "components/service/secure_storage/frontend/secure_storage_provider/secure_storage_uuid.h"
 #include "sp_api.h"
 #include "sp_discovery.h"
 #include "sp_messaging.h"
@@ -20,13 +21,15 @@ static uint8_t rx_buffer[4096] __aligned(4096);
 void sp_main(union ffa_boot_info *boot_info)
 {
 	sp_result result = SP_RESULT_INTERNAL_ERROR;
-	struct rpc_interface *secure_storage_iface = NULL;
-	struct ffa_call_ep ffa_call_ep = { 0 };
+	struct rpc_service_interface *secure_storage_iface = NULL;
+	struct ts_rpc_endpoint_sp rpc_endpoint = { 0 };
 	struct sp_msg req_msg = { 0 };
 	struct sp_msg resp_msg = { 0 };
 	struct secure_storage_provider secure_storage_provider = { 0 };
 	struct storage_backend *storage_backend = NULL;
 	uint16_t own_id = 0;
+	const struct rpc_uuid service_uuid = { .uuid = TS_PSA_PROTECTED_STORAGE_UUID };
+	rpc_status_t rpc_status = RPC_ERROR_INTERNAL;
 
 	/* Boot */
 	(void)boot_info;
@@ -50,13 +53,23 @@ void sp_main(union ffa_boot_info *boot_info)
 	}
 
 	secure_storage_iface = secure_storage_provider_init(&secure_storage_provider,
-							    storage_backend);
+							    storage_backend, &service_uuid);
 	if (!secure_storage_iface) {
 		EMSG("Failed to init secure storage provider");
 		goto fatal_error;
 	}
 
-	ffa_call_ep_init(&ffa_call_ep, secure_storage_iface, own_id);
+	rpc_status = ts_rpc_endpoint_sp_init(&rpc_endpoint, 1, 16);
+	if (rpc_status != RPC_SUCCESS) {
+		EMSG("Failed to initialize RPC endpoint: %d", rpc_status);
+		goto fatal_error;
+	}
+
+	rpc_status = ts_rpc_endpoint_sp_add_service(&rpc_endpoint, secure_storage_iface);
+	if (rpc_status != RPC_SUCCESS) {
+		EMSG("Failed to add service to RPC endpoint: %d", rpc_status);
+		goto fatal_error;
+	}
 
 	/* End of boot phase */
 	result = sp_msg_wait(&req_msg);
@@ -66,7 +79,7 @@ void sp_main(union ffa_boot_info *boot_info)
 	}
 
 	while (1) {
-		ffa_call_ep_receive(&ffa_call_ep, &req_msg, &resp_msg);
+		ts_rpc_endpoint_sp_receive(&rpc_endpoint, &req_msg, &resp_msg);
 
 		result = sp_msg_send_direct_resp(&resp_msg, &req_msg);
 		if (result != SP_RESULT_OK) {
