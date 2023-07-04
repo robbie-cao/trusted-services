e4367bb06f1e617ae98fc598351625395ac10aa6
Imre Kis <imre.kis@arm.com>
2023-07-04 13:55:14 +0200

    Align dummy caller with the new RPC design
    Implement rpc_caller_interface in dummy caller.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I0e5dff1ea8c42ce17357ffd3542e3a2166567c8e

--- components/rpc/dummy/dummy_caller.h
+++ components/rpc/dummy/dummy_caller.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -7,7 +7,7 @@
 #ifndef DUMMY_CALLER
 #define DUMMY_CALLER
 
-#include <rpc_caller.h>
+#include "rpc_caller.h"
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -22,17 +22,10 @@ extern "C" {
  * established but a client doesn't wish to treat the condition
  * as a fatal error.
  */
-struct dummy_caller
-{
-    struct rpc_caller rpc_caller;
-    rpc_status_t rpc_status;
-    rpc_opstatus_t opstatus;
-    uint8_t *req_buf;
-};
 
-struct rpc_caller *dummy_caller_init(struct dummy_caller *s,
-                                    rpc_status_t rpc_status, rpc_opstatus_t opstatus);
-void dummy_caller_deinit(struct dummy_caller *s);
+rpc_status_t dummy_caller_init(struct rpc_caller_interface *caller, rpc_status_t rpc_status,
+			       service_status_t service_status);
+rpc_status_t dummy_caller_deinit(struct rpc_caller_interface *caller);
 
 #ifdef __cplusplus
 }
