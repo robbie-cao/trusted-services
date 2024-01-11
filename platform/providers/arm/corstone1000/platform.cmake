bb361bf4b9541e60df930cdf8b7964cbc2818f47
Bence Balogh <bence.balogh@arm.com>
2024-01-11 10:02:27 +0100

    Decrease SMM_GATEWAY_MAX_UEFI_VARIABLES for CS1000
    This fixes the SMM gateway initialization error that was caused by a
    malloc fault in Corstone-1000.
    
    Signed-off-by: Bence Balogh <bence.balogh@arm.com>
    Change-Id: I76296ddcf51dd879f3432ff36b96e3a712324f62

--- platform/providers/arm/corstone1000/platform.cmake
+++ platform/providers/arm/corstone1000/platform.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2021-2023, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2021-2024, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -11,5 +11,5 @@ include(${TS_ROOT}/platform/drivers/arm/mhu_driver/component.cmake)
 
 target_compile_definitions(${TGT} PRIVATE
 	SMM_VARIABLE_INDEX_STORAGE_UID=0x787
-	SMM_GATEWAY_MAX_UEFI_VARIABLES=100
+	SMM_GATEWAY_MAX_UEFI_VARIABLES=80
 )
